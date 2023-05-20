5247bf82f1fa33323eaa7937a2bc8e9d0d7ac842
Daniel Don <dondaniel028@gmail.com>
2023-05-20 16:12:48 +0000

    Update 2 files

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     println!("Enter the temperature in Fahrenheit");
     let mut input: String = String::new();
     stdin().read_line(&mut input).expect("Error entering value");
-    let farh: f64 =  input.trim().parse().expect("You didn't enter a number!");
+    let farh: f64 =  input.trim().parse().expect("You didn't enter a number, I'm sorry, you're going to have to!");
     let celcius = ((farh - 32.0) * 5.0 / 9.0).floor();
 
     println!("{} Fahrenheit approximated in Celcius is {} degrees!", farh, celcius)  
